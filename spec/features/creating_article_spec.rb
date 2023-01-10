b6073818c9a9fd624ec90e6fcc4380ad11c4950d
kshakir1989 <kshakir1989@gmail.com>
2023-01-10 07:13:53 -0500

    created first rspec test

---
+++ spec/features/creating_article_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.featrue "Create Articles" do
+    scenario "A user creates a new article" do 
+        visit "/" # Go to root
+        click_link "New Article" # Go to new article link 
+        
+        fill_in "Title" with: "Creating a blog" # Fill in the title field with "Creating a blog"
+        fill_in "Body" with: "Body text here" # Fill in the body aread with text
+
+        click_button "Create Article" # Click create article button 
+
+        expect(page).to have_content("Article has been created") # Set expectation after creating article
+        expect(page.current_path).to eq(articles_path)
+    end
+end
